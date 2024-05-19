1e624b9d15973b7098a053bd1afeacd0e0fafca4
Azimov Vyacheslav <96425986+azim00ff@users.noreply.github.com>
2024-05-19 10:18:17 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     ],
     author='azim0ff',
     author_email='azimoff@azimoff.online',
-    description='Библиотека для работы с API RakHub, позволяющая удобно запускать RakBot сессии на игроков San Andreas Multiplayer (SAMP).',
+    description='Библfиотека для работы с API RakHub, позволяющая удобно запускать RakBot сессии на игроков San Andreas Multiplayer (SAMP).',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/azim00ff/RakHubAPI/',
